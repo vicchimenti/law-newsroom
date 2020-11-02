9f99b559dd440391ebc156b33cc6bc02314375e9
Vic Chimenti <chimenti@seattleu.edu>
2020-11-02 14:51:39 -0800

    ud to cat

--- law-organizer-korematsuCenter.js
+++ law-organizer-korematsuCenter.js
@@ -1,6 +1,6 @@
 /* law/organizer/korematsuCenter */
 
-var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Categories" output="normal" display_field="value" modifiers="htmlentities" delimiter=";" />'); //edit the field name to change the field
+var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" modifiers="htmlentities" delimiter=";" />'); //edit the field name to change the field
 var optionToTestFor = "korematsuCenter"; //edit this to change the option
 var contentTypeLayout   = 'output/categoryfeed'; //edit this to change the Content Layout to use for output
 

8b0bb5732df4901da313e3724a3bcd644517d82e
Chimenti <chimenti@seattleu.edu>
2020-11-25 11:39:36 -0800

    add bs panel group

--- organizer-law-newsroom-major.js
+++ organizer-law-newsroom-major.js
@@ -24,7 +24,7 @@ try {
     switch (choice) {
         default: header = '\
 			<div class="newsroomOrganizerWrapper col-xs-12 order-xs-1 col-md-8 col-md-push-4 order-md-2" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
-					<div class="newsroomOrganizer standardContent card-group">\
+					<div class="newsroomOrganizer standardContent card-group border border-left border-dark">\
                   		<div class="newsroomOrganizerExtra"></div>\
 		';
         midder = '\

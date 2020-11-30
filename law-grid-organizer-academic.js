f743eaddb573886889107c4d75348d41938edfc9
Vic Chimenti <chimenti@seattleu.edu>
2020-11-30 11:01:06 -0800

    use grid organizer as switcher to output

--- law-grid-organizer-academic.js
+++ law-grid-organizer-academic.js
@@ -1,118 +1,34 @@
-/***
- *     @author Victor Chimenti, MSCS-SE '20
- *     @file law-grid-organizer-academic.js
- *     @see Seattle University School of Law Newsroom
- *
- *     This new content type layout is a smart layout for academic category feed
- *     news items that must obey a masonry grid layout. In its first iteration
- *     this layout will be dedicated to the category news pages.
- *
- *     This content layout will be the organizer layout and will link to the
- *     full text layout to reveal the full article.
- *
- *     Document will write once when the page loads
- *
- *     @version 3.0
- */
+/* law/organizer/academic */
+
+//var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" modifiers="htmlentities" delimiter=";" />'); //edit the field name to change the field
+
+
+var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
+
+
+var optionToTestFor = "academic"; //edit this to change the option
+var contentTypeLayout   = 'output/categoryfeed'; //edit this to change the Content Layout to use for output
+
+var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
+
+//document.write('<p>fieldToBeEvaluated: ' + fieldToBeEvaluated + "<br>");
+//document.write('n: ' + n + "<br>");
+
 
 try {
+  if ((n >= 0)){ /* if it's there, it'll start at 0 or later, so process this */
+	var sw = new java.io.StringWriter();
+	var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
+	new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview); 
+	output = sw.toString();
 
-    /***
-     *  Assign local variables from the content type's fields
-     * 
-     * */
-    var articleTitle = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Title' output='normal' display_field='value' />");
-    var headline = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Headline' output='normal' display_field='value' />");
-    var frontPageImage = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Media Library Image' output='normal' formatter='path/*' />");
-    var frontPageImagCaption = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Media Library Image Caption' output='normal' modifiers='striptags,htmlentities' />");
-    var frontPageImagCredit = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Media Library Image Credit' output='normal' modifiers='striptags,htmlentities' />");
-    var articleSummary = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Abstract' output='normal' display_field='value' />");
-    var articleFullBody = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Body' output='normal' display_field='value' />");
-    var author = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Author' output='normal' display_field='value' />");
-    var publishedDate = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Publish Date' output='normal' date_format='MMMM d, yyyy' />");
-    var fullTextLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Name' output='fulltext' use-element='true' filename-element='Name' modifiers='striptags,htmlentities' />");
-    var fieldTags = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Category' output='normal' display_field='name' />");
-    var pinned = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Pinned' output='normal' display_field='value' />");
+	document.write(output);
 
-
-
-
-    /***
-     *  Declare/Assign local variables with base formatting
-     * 
-     * */
-    var thumbNailString = '<span class="newsroomImageWrapper"><img src="' + frontPageImage + '" class="articleImage card-img-top" alt="' + frontPageImagCaption + '" /></span>';
-    var listOfTags = "";
-    var titleLink = "";
-    var authorByLine = "";
-    var beginningHTML = '<div class="gridFeedItem newsroomArticleWrapper newsroomBlurb card" id="id<t4 type=\'meta\' meta=\'content_id\' data-position-default="Main" data-position-selected="Main"/>">';
-    var endingHTML = '<hr class="articleBorderBottom"></div>';
-
-
-
-
-    /***
-     *  parse the list of tags, add <li> tags
-     * 
-     * */
-    if (fieldTags != "") {
-        var arrayOfTags = fieldTags.split(',');
-        for (let i = 0; i < arrayOfTags.length; i++) {
-            listOfTags += '<li class="tag">' + arrayOfTags[i] + '</li>';
-        }
-        listOfTags = '<div class="newsroomArticle tags"><ul class="categories">' + listOfTags + '</ul></div>';
-    }
-
-
-    /***
-     *  determine which link, if any, goes in the title
-     *  and determine if a unique headline was added
-     *  if no headline is added then use the title
-     * 
-     * */
-    if (headline == "" && articleFullBody == "") {
-        titleLink = '<h3 class="newsroomArticleTitle card-title">' + articleTitle + '</h3>';
-    } else if (articleFullBody == "") {
-        titleLink = '<h3 class="newsroomArticleTitle card-title">' + headline + '</h3>';
-    } else if (headline == "") {
-        titleLink = '<h3 class="newsroomArticleTitle card-title"><a href="' + fullTextLink + '">' + articleTitle + '</a></h3>';
-    } else {
-        titleLink = '<h3 class="newsroomArticleTitle card-title"><a href="' + fullTextLink + '">' + headline + '</a></h3>';
-    }
-
-
-    /***
-     *  display byline only when provided 
-     * 
-     * */
-    if (author == "") {
-        authorByLine = '<p class="byLine hidden">No Author Provided</p>';
-    } else {
-        authorByLine = '<p class="byLine">By ' + author + '</p>';
-    }
-
-
-
-
-    /***
-     *  Write the document once
-     * 
-     * */
-    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, beginningHTML));
-    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, thumbNailString));
-    document.write('<div class="newsroomArticleBlurb container card-body"><div class="row">');
-    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, titleLink));
-    document.write('<span class="newsroomArticleLead card-text">' + articleSummary + '</span>');
-    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, authorByLine));
-    document.write('<p class="newsroomArticlePublishedDate">' + publishedDate + '</p>');
-    document.write(listOfTags);
-    document.write('<div class="hidden"><span class="articlePinned">' + pinned + '</span></div>');
-    document.write('</div></div>'); // close newsroomArticleBlurb and row divs
-    document.write(endingHTML);
-
-
-
-
-} catch (err) {
-    document.write(err.message);
+  }
+  else{
+//document.write("else<br>");
+  }
+//document.write("</p>");
+} catch(err) {
+	document.write(err.message);
 }

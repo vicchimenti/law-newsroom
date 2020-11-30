926183880541d42fe16e41281cecea673052ffa6
Vic Chimenti <chimenti@seattleu.edu>
2020-11-30 11:56:45 -0800

    use legacy option to test for

--- law-grid-organizer-academic.js
+++ law-grid-organizer-academic.js
@@ -11,11 +11,11 @@
 var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
 var optionToTestFor = "academic"; //edit this to change the option
 var contentTypeLayout   = 'output/gridfeed'; //edit this to change the Content Layout to use for output
-// var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
+var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
 
 
 try {
-    if (fieldToBeEvaluated == optionToTestFor){ /* if it's there, it'll start at 0 or later, so process this */
+    if ((n >= 0)) { /* if it's there, it'll start at 0 or later, so process this */
         var sw = new java.io.StringWriter();
         var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
         new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview); 

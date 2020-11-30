2be1f7b4b65db8e0c41c58fcd99296509e02b344
Vic Chimenti <chimenti@seattleu.edu>
2020-11-30 11:06:49 -0800

    refact switcher

--- law-grid-organizer-academic.js
+++ law-grid-organizer-academic.js
@@ -1,34 +1,31 @@
-/* law/organizer/academic */
-
-//var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" modifiers="htmlentities" delimiter=";" />'); //edit the field name to change the field
-
-
-var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
+/***
+ *  law/grid/organizer/academic
+ * 
+ * */
 
 
+
+
+var fieldToBeEvaluated = content.get("Category").publish(); //edit this to change the field
+
+// var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
 var optionToTestFor = "academic"; //edit this to change the option
-var contentTypeLayout   = 'output/categoryfeed'; //edit this to change the Content Layout to use for output
-
-var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
-
-//document.write('<p>fieldToBeEvaluated: ' + fieldToBeEvaluated + "<br>");
-//document.write('n: ' + n + "<br>");
+var contentTypeLayout   = 'output/gridfeed'; //edit this to change the Content Layout to use for output
+// var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
 
 
 try {
-  if ((n >= 0)){ /* if it's there, it'll start at 0 or later, so process this */
-	var sw = new java.io.StringWriter();
-	var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
-	new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview); 
-	output = sw.toString();
+    if (fieldToBeEvaluated == optionToTestFor){ /* if it's there, it'll start at 0 or later, so process this */
+        var sw = new java.io.StringWriter();
+        var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
+        new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview); 
+        output = sw.toString();
 
-	document.write(output);
 
-  }
-  else{
-//document.write("else<br>");
-  }
-//document.write("</p>");
+        document.write(output);
+    }
+
+
 } catch(err) {
 	document.write(err.message);
 }

ebf9560714d7f924e07ac644dcb019fdf20563d7
Vic Chimenti <chimenti@seattleu.edu>
2020-12-03 17:26:32 -0800

    ud v

--- organizer-law-newsroom-base.js
+++ organizer-law-newsroom-base.js
@@ -14,7 +14,7 @@
  *
  *      Adapted from the existing organizer organizer.js media library id 163514
  *
- *      @version 3.5
+ *      @version 3.6
  */
 
 importClass(com.terminalfour.sitemanager.cache.CachedContent);

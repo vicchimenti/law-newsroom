516dbf1a69efe444f0013f85c7832b9f3b9ce29e
Vic Chimenti <chimenti@seattleu.edu>
2020-12-04 09:42:28 -0800

    from publish to getValue

--- organizer-law-newsroom-base.js
+++ organizer-law-newsroom-base.js
@@ -275,8 +275,11 @@ function tagSort(tag, elem) {
 
     return function(a, b) {
         // assign values from the element to a string for boolean comparison
-        let valueA = a.Content.get(elem).publish();
-        let valueB = b.Content.get(elem).publish();
+        let valueA = a.Content.get(elem).getValue();
+        let valueB = b.Content.get(elem).getValue();
+
+        // let valueA = a.Content.get(elem).publish();
+        // let valueB = b.Content.get(elem).publish();
         log("valueA: " + valueA);
         log("valueB: " + valueB);
         log("tag: " + tag);

1facf08a012ac42231088bb68b30923b02b1ee4e
Vic Chimenti <chimenti@seattleu.edu>
2020-12-07 15:51:57 -0800

    ud homeless cat

--- law-organizer-homelessRightsAdvocacyProject.js
+++ law-organizer-homelessRightsAdvocacyProject.js
@@ -1,25 +1,37 @@
-/* law/organizer/homelessRightsAdvocacyProject */
+/***
+ *  law/organizer/homelessRightsAdvocacyProject
+ * 
+ */
 
-var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" modifiers="htmlentities" delimiter=";" />'); //edit the field name to change the field
+
+/***
+ *  declare and assign topic layout
+ * 
+ */
+var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
 var optionToTestFor = "homelessRightsAdvocacyProject"; //edit this to change the option
-var contentTypeLayout   = 'output/categoryfeed'; //edit this to change the Content Layout to use for output
-
+var contentTypeLayout = 'output/categoryfeed'; //edit this to change the Content Layout to use for output
 var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
 
+
+/***
+ *  send correct layout to the document
+ * 
+ */
 try {
-  if ((n >= 0)){ /* if it's there, it'll start at 0 or later, so process this */
-	var sw = new java.io.StringWriter();
-	var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
-	new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview); 
-	output = sw.toString();
 
-	document.write(output);
+    /* if content exists, it'll start at 0 or later, so process this */
+    if ((n >= 0)) {
+        var sw = new java.io.StringWriter();
+        var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
+        new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview);
+        output = sw.toString();
 
-  }
-  else{
+        // write to page document
+        document.write(output);
+    }
 
-  }
 
-} catch(err) {
-	document.write(err.message);
+} catch (err) {
+    document.write(err.message);
 }

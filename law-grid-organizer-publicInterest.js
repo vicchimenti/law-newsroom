8381d1da7984ae646ecf9f27d3271ff40bda41b9
Vic Chimenti <chimenti@seattleu.edu>
2020-12-07 15:28:55 -0800

    ud selector

--- law-grid-organizer-publicInterest.js
+++ law-grid-organizer-publicInterest.js
@@ -1,5 +1,5 @@
 /***
- *  law/grid/organizer/proBono
+ *  law/grid/organizer/publicInterest
  * 
  */
 
@@ -9,7 +9,7 @@
  * 
  */
 var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
-var optionToTestFor = "proBono"; //edit this to change the option
+var optionToTestFor = "publicInterest"; //edit this to change the option
 var contentTypeLayout = 'output/gridfeed'; //edit this to change the Content Layout to use for output
 var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
 

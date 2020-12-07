2975851e2fc68901fa98297e09a7930855669ee6
Vic Chimenti <chimenti@seattleu.edu>
2020-12-07 14:52:15 -0800

    add diversity grid

---
+++ law-grid-organizer-diversity.js
@@ -0,0 +1,35 @@
+/***
+ *  law/grid/organizer/diversity
+ * 
+ */
+
+
+/***
+ *  declare and assign topic layout
+ * 
+ */
+var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
+var optionToTestFor = "diversity"; //edit this to change the option
+var contentTypeLayout = 'output/gridfeed'; //edit this to change the Content Layout to use for output
+var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
+
+
+/***
+ *  send correct layout to the document
+ * 
+ */
+try {
+    if ((n >= 0)) { /* if it's there, it'll start at 0 or later, so process this */
+        var sw = new java.io.StringWriter();
+        var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
+        new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview);
+        output = sw.toString();
+
+        document.write(output);
+
+    } else {}
+
+
+} catch (err) {
+    document.write(err.message);
+}

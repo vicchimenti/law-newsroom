a95f341ce2678612d8bbf71ecb881ba436c1638d
Vic Chimenti <chimenti@seattleu.edu>
2020-12-10 16:02:12 -0800

    prep for suLawInTheNews feed

--- law-organizer-suLawInTheNews.js
+++ law-organizer-suLawInTheNews.js
@@ -10,7 +10,7 @@
  */
 var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
 var optionToTestFor = "suLawInTheNews"; //edit this to change the option
-var contentTypeLayout = 'output/categoryfeed'; //edit this to change the Content Layout to use for output
+var contentTypeLayout = 'output/suLawInTheNewsfeed'; //edit this to change the Content Layout to use for output
 var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
 
 

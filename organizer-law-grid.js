b4928dfe5bf56502e90afea307246febe30afe9a
Vic Chimenti <chimenti@seattleu.edu>
2020-12-14 13:13:22 -0800

    add contentItem to grid organizer

--- organizer-law-grid.js
+++ organizer-law-grid.js
@@ -19,7 +19,7 @@ try {
     var header, midder, footer;
 
     // declare content wrappers
-    header = '<div class="gridOrganizerWrapper" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main"><div class="gridOrganizer standardContent card-group"><div class="gridOrganizerExtra"></div>';
+    header = '<div class="gridOrganizerWrapper contentItem" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main"><div class="gridOrganizer standardContent card-group"><div class="gridOrganizerExtra"></div>';
     midder = '<span></span>';
     footer = '</div><div class="gridOrganizer ToggleExtra boxlinks" style="display:none">Show More</div></div>';
 

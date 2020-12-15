aca6d15bdaebda12cef6559b86f8c4fa56bd7865
Vic Chimenti <chimenti@seattleu.edu>
2020-12-14 16:16:34 -0800

    ud order md and xs

--- organizer-law-newsroom-minor.js
+++ organizer-law-newsroom-minor.js
@@ -23,7 +23,7 @@ try {
     var choice = String(content.get('Article type').publish());
     switch (choice) {
         default: header = '\
-			<div class="newsroomOrganizerWrapper col-xs-12 order-xs-2 col-md-4 col-md-pull-8 order-md-first" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
+			<div class="newsroomOrganizerWrapper col-xs-12 order-xs-3 col-md-4 col-md-pull-8 order-md-2" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
 				<div class="newsroomOrganizer standardContent">\
                   <div class="newsroomOrganizerExtra"></div>\
 		';

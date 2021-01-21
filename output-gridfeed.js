d2314d5b2e136a566d2c2ae35522f57a09c6bba2
vicchimenti <victor.chimenti@gmail.com>
2021-01-21 11:26:49 -0800

    add footer and ud closing divs

--- output-gridfeed.js
+++ output-gridfeed.js
@@ -151,10 +151,11 @@ try {
     document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, titleLink));
     document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, cardText));
     document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, authorByLine));
-    document.write('<p class="newsroomArticlePublishedDate">' + publishedDate + '</p>');
+    document.write('</div>'); // close row
+    document.write('<div class="card-footer"><small class="text-muted newsroomArticlePublishedDate">' + publishedDate + '</small></div>');
     document.write(listOfTags);
     document.write('<div class="hidden"><span class="articlePinned">' + pinned + '</span><span class="catPinned">' + catPin + '</span></div>');
-    document.write('</div></div>'); // close newsroomArticleBlurb and row divs
+    document.write('</div>'); // close newsroomArticleBlurb
     document.write(endingHTML);
 
 

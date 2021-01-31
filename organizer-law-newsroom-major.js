43310edbf78d0611e22bec66ae8b7de903d85c64
vicchimenti <victor.chimenti@gmail.com>
2021-01-31 09:07:23 -0800

    ud major to card deck

--- organizer-law-newsroom-major.js
+++ organizer-law-newsroom-major.js
@@ -24,7 +24,7 @@ try {
     switch (choice) {
         default: header = '\
 			<div class="newsroomOrganizerWrapper col-xs-12 order-xs-2 col-md-8 col-md-push-4 order-md-3" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
-					<div class="newsroomOrganizer standardContent card-group border border-left border-dark">\
+					<div class="newsroomOrganizer standardContent card-deck border border-left border-dark">\
                   		<div class="newsroomOrganizerExtra"></div>\
 		';
         midder = '\

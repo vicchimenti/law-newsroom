5b41e448ba72c06eec325bd34a7b5fd9e229d5e4
vicchimenti <victor.chimenti@gmail.com>
2021-01-31 09:23:37 -0800

    add col 12

--- organizer-law-newsroom-major.js
+++ organizer-law-newsroom-major.js
@@ -23,7 +23,7 @@ try {
     var choice = String(content.get('Article type').publish());
     switch (choice) {
         default: header = '\
-			<div class="newsroomOrganizerWrapper major-feed col-xs-12 order-xs-2 col-md-8 col-md-push-4 order-md-3" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
+			<div class="newsroomOrganizerWrapper major-feed add col-12 col-xs-12 order-xs-2 col-md-8 col-md-push-4 order-md-3" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
 					<div class="newsroomOrganizer standardContent card-deck border-0 border-start">\
                   		<div class="newsroomOrganizerExtra"></div>\
 		';

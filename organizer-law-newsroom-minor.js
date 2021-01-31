e0fa3c59508553ca5f5ec68161d9f5d660201d20
vicchimenti <victor.chimenti@gmail.com>
2021-01-31 09:07:54 -0800

    ud minor to card deck

--- organizer-law-newsroom-minor.js
+++ organizer-law-newsroom-minor.js
@@ -24,7 +24,7 @@ try {
     switch (choice) {
         default: header = '\
 			<div class="newsroomOrganizerWrapper col-xs-12 order-xs-3 col-md-4 col-md-pull-8 order-md-2" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
-				<div class="newsroomOrganizer standardContent">\
+				<div class="newsroomOrganizer standardContent card-deck">\
                   <div class="newsroomOrganizerExtra"></div>\
 		';
         midder = '\

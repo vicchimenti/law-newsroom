b2288a2b99e84952aca7ffd1edbe064c23d91254
vicchimenti <victor.chimenti@gmail.com>
2021-01-31 09:21:59 -0800

    add col 12 and minor feed classes

--- organizer-law-newsroom-minor.js
+++ organizer-law-newsroom-minor.js
@@ -23,7 +23,7 @@ try {
     var choice = String(content.get('Article type').publish());
     switch (choice) {
         default: header = '\
-			<div class="newsroomOrganizerWrapper col-xs-12 order-xs-3 col-md-4 col-md-pull-8 order-md-2" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
+			<div class="newsroomOrganizerWrapper minor-feed col-12 col-xs-12 order-xs-3 col-md-4 col-md-pull-8 order-md-2" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
 				<div class="newsroomOrganizer standardContent card-deck border-0">\
                   <div class="newsroomOrganizerExtra"></div>\
 		';

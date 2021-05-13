2df6636d24c239a873d74952c88be349d261add7
vicchimenti <chimenti@seattleu.edu>
2021-05-13 06:28:19 -0700

    ud remarks

--- law-organizer-archives-categoryfeed.js
+++ law-organizer-archives-categoryfeed.js
@@ -1,19 +1,20 @@
 /***
- *     @author Victor Chimenti, MSCS-SE '20
- *     @file output-categoryfeed.js
- *     @see Seattle University School of Law Newsroom
+ *      @author Victor Chimenti, MSCS-SE '20
+ *      @file law-organizer-archives-categoryfeed.js
+ *      @see Seattle University School of Law Newsroom
+ *      law/organizer/archives/categoryfeed
  *
- *     This new content type layout is a smart layout for all items in the
- *     category newsfeed used by the School of Law Newsroom. This content type
- *     ensures that images, headlines, by-lines, tags and the abstract
- *     summary layout as expected.
+ *      This new content type layout is a smart layout for all items in the
+ *      category newsfeed used by the School of Law Newsroom. This content type
+ *      ensures that images, headlines, by-lines, tags and the abstract
+ *      summary layout as expected.
  *
- *     This content layout will be the organizer layout and will link to the
- *     full text layout to reveal the full article.
+ *      This content layout will be the organizer layout and will link to the
+ *      full text layout to reveal the full article.
  *
- *     Document will write once when the page loads
+ *      Document will write once when the page loads
  *
- *     @version 5.5
+ *      @version 5.5
  */
 
 

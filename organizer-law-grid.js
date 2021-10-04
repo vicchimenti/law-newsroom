59cae4dcdc15652147a66e2eed9b530dcec462dd
vicchimenti <victor.chimenti@gmail.com>
2021-10-04 15:07:03 -0700

    udbase id

--- organizer-law-grid.js
+++ organizer-law-grid.js
@@ -12,7 +12,7 @@ function readMedia(mediaID) {
 
 try {
     // Import Organizer base from media library
-    var base = readMedia(2686600);
+    var base = readMedia(3045187);
     eval(String(base));
 
     var titleField = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Title" output="selective-output" modifiers="striptags,htmlentities" format="$value" />');

fe0d69048d9aa7c0d7b7605cac79608cc8bb3ba6
vicchimenti <victor.chimenti@gmail.com>
2021-10-28 08:40:55 -0700

    parse by image element

--- law-organizer-news-major.js
+++ law-organizer-news-major.js
@@ -6,12 +6,16 @@
  */
 
 
-var fieldToBeEvaluated = content.get("Newscenter Homepage").publish(); //edit this to change the field
-var optionToTestFor = "major"; //edit this to change the option
-var contentTypeLayout = 'output/majorfeed'; //edit this to change the Content Layout to use for output
+let fieldToBeEvaluated = content.get("Newscenter Homepage").publish(); //edit this to change the field
+let optionToTestFor = "major"; //edit this to change the option
+let contentTypeLayout = 'output/majorfeed'; //edit this to change the Content Layout to use for output
+
+// validate item has image before calling writing to major feed
+let imageElement = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Main Image' output='normal' formatter='path/*' />");
+
 
 try {
-    if (fieldToBeEvaluated == optionToTestFor) {
+    if (fieldToBeEvaluated == optionToTestFor && imageElement != "") {
         var sw = new java.io.StringWriter();
         var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
         new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview);

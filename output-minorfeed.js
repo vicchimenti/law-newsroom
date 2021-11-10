b902440c347159a0f36428550d6ba21dda6dcaef
vicchimenti <victor.chimenti@gmail.com>
2021-11-10 10:17:07 -0800

    full body

--- output-minorfeed.js
+++ output-minorfeed.js
@@ -10,7 +10,7 @@
  *
  *     Document will write once when the page loads
  *
- *     @version 2.5
+ *     @version 2.6
  */
 
 
@@ -224,9 +224,9 @@ try {
      *  determine if the article contains full text content
      * 
      * */
-    let titleLink = (minorDict.articleFullBody.content && minorDict.headline.content)
+    let titleLink = (minorDict.articleFullBody.content)
                     ? '<h3 class="newsroomArticleTitle card-title"><a href="' + minorDict.fullTextLink.content + '" class="card-link" aria-label="Read the full article at: ' + minorDict.headline.content + '" >' + minorDict.headline.content + '</a></h3>'
-                    : '<h3 class="newsroomArticleTitle card-title">' + minorDict.itemName.content + '</h3>';
+                    : '<h3 class="newsroomArticleTitle card-title">' + minorDict.headline.content + '</h3>';
 
 
 

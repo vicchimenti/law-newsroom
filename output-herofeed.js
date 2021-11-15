98eb6ba9c4970b61708511778c63e62dde441765
vicchimenti <victor.chimenti@gmail.com>
2021-11-15 15:47:33 -0800

    udr

--- output-herofeed.js
+++ output-herofeed.js
@@ -1,19 +1,13 @@
 /***
- *     @author Victor Chimenti, MSCS-SE '20
- *     @file output-herofeed.js
- *     @see Seattle University School of Law Newsroom
+ *      @author Victor Chimenti, MSCS
+ *      @see Seattle University School of Law Newsroom
+ *      @file output-herofeed.js
+ *          Law - Newscenter Story
+ *          ID: 5296
  *
- *     This new content type layout is a smart layout for all items in the
- *     major newsfeed used by the School of Law Newsroom. This content type
- *     ensures that images, headlines, by-lines, tags and the abstract
- *     summary layout as expected.
+ *      Document will write once when the page loads
  *
- *     This content layout will be the organizer layout and will link to the
- *     full text layout to reveal the full article.
- *
- *     Document will write once when the page loads
- *
- *     @version 3.4
+ *      @version 3.4
  */
 
 

e701da050f23d28aa9851e13ef77b5b0d7be618d
vicchimenti <victor.chimenti@gmail.com>
2021-11-16 10:39:42 -0800

    primary and secondary contacts

--- law-text-html-contact.js
+++ law-text-html-contact.js
@@ -1,10 +1,19 @@
 <div class="newsCenterContact card">
   <h3 class="newsCenterContactHeader card-header"></h3>
   <div class="newsCenterContactBody card-body">
-    <h4 class="card-title contactName"></h4>
-    <p class="card-text contactTitle"></p>
-    <p class="card-text contactDepartment"></p>
-    <p class="card-text emailWrapper"><a href="#" class="contactEmail"></a></p>
-    <p class="card-text contactPhone"></p> 
+      <div class="newsCenterContactPrimary">
+        <h4 class="card-title contactName"></h4>
+        <p class="card-text contactTitle"></p>
+        <p class="card-text contactDepartment"></p>
+        <p class="card-text emailWrapper"><a href="#" class="contactEmail"></a></p>
+        <p class="card-text contactPhone"></p> 
+      </div>
+      <div class="newsCenterContactSecondary">
+        <h4 class="card-title contactName"></h4>
+        <p class="card-text contactTitle"></p>
+        <p class="card-text contactDepartment"></p>
+        <p class="card-text emailWrapper"><a href="#" class="contactEmail"></a></p>
+        <p class="card-text contactPhone"></p> 
+      </div>
   </div>
 </div>

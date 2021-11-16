6e272f2e6c63bb26493fb186bfbb2d9094b01195
vicchimenti <victor.chimenti@gmail.com>
2021-11-16 10:36:46 -0800

    base html

---
+++ law-text-html-contact.js
@@ -0,0 +1,10 @@
+<div class="newsCenterContact card">
+  <h3 class="newsCenterContactHeader card-header"></h3>
+  <div class="newsCenterContactBody card-body">
+    <h4 class="card-title contactName"></h4>
+    <p class="card-text contactTitle"></p>
+    <p class="card-text contactDepartment"></p>
+    <p class="card-text emailWrapper"><a href="#" class="contactEmail"></a></p>
+    <p class="card-text contactPhone"></p> 
+  </div>
+</div>

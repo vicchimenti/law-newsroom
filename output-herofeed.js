1495838bf814e98177f5b56d6610c45390c54581
vicchimenti <victor.chimenti@gmail.com>
2021-11-15 16:35:20 -0800

    clean

--- output-herofeed.js
+++ output-herofeed.js
@@ -112,24 +112,6 @@ try {
 
     }
 
-    /***
-     *  Assign local variables from the content type's fields
-     * 
-     * */
-    // var articleTitle = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Title' output='normal' display_field='value' />");
-    // var headline = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Headline' output='normal' display_field='value' />");
-    // var frontPageImage = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Main Image' output='normal' formatter='path/*' />");
-    // var frontPageImageCaption = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Main Image Caption' output='normal' modifiers='striptags,htmlentities' />");
-    // var frontPageImageCredit = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Main Image Credit' output='normal' modifiers='striptags,htmlentities' />");
-    // var articleSummary = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Abstract' output='normal' display_field='value' />");
-    // var articleFullBody = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Article Body' output='normal' display_field='value' />");
-    // var author = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Author' output='normal' display_field='value' />");
-    // var publishedDate = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Publish Date' output='normal' date_format='MMMM d, yyyy' />");
-    // var fullTextLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Headline' output='fulltext' use-element='true' filename-element='Headline' modifiers='striptags,htmlentities' />");
-    // var fieldTags = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Category' output='normal' display_field='name' />");
-    // var pinned = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Pinned' output='normal' display_field='value' />");
-    // var anchorTag = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='meta' meta='html_anchor' />"); 
-
 
 
 
@@ -137,12 +119,6 @@ try {
      *  Declare/Assign local variables with base formatting
      * 
      * */
-    // var listOfTags = "";
-    // var titleLink = "";
-    // var listItems = "";
-    // var listOfTags = "";
-    // var photoCredit = "";
-    // var authorByLine = "";
     let openCardBody = '<div class="newsroomArticleBlurb container card-body"><div class="row px-0">';
     let closeCardBody = '</div></div>';
     let openHidden = '<div class="searchSortFields visually-hidden">';
@@ -150,7 +126,6 @@ try {
     let imageString = '<span class="imageString hidden visually-hidden" />No Image Provided</span>';
     let openImageWrapper = '<figure class="figure hidden visually-hidden">';
     let closeImageWrapper = '</figure>';
-    // var imageString = '<span class="newsroomImageWrapper"><img src="' + frontPageImage + '" class="articleImage card-img-top" alt="' + frontPageImageCaption + '" /></span>';
     let beginningHTML = '<article class="newsroomHeroFeedItem newsroomBlurb col-12 col-xs-12 card border-0" id="hero' + heroDict.contentId.content + '" aria-label="' + heroDict.headline.content + '">';
     let endingHTML = '<hr class="articleBorderBottom"></article>';
 
@@ -180,69 +155,6 @@ try {
 
 
     /***
-     *  parse the list of tags, add <li> tags
-     * 
-     * */
-    // if (fieldTags != "") {
-    //     var arrayOfTags = fieldTags.split(',');
-    //     for (let i = 0; i < arrayOfTags.length; i++) {
-    //         listItems += '<li class="tag">' + arrayOfTags[i] + '</li>';
-    //     }
-    //     listOfTags = '<div class="newsroomArticle tags"><ul class="categories">' + listItems + '</ul></div>';
-    // }
-
-
-    /***
-     *  determine which link, if any, goes in the title
-     *  and determine if a unique headline was added
-     *  if no headline is added then use the title
-     * 
-     * */
-    // if (headline == "" && articleFullBody == "") {
-    //     titleLink = '<h3 class="newsroomArticleTitle card-title">' + articleTitle + '</h3>';
-    // } else if (articleFullBody == "") {
-    //     titleLink = '<h3 class="newsroomArticleTitle card-title">' + headline + '</h3>';
-    // } else if (headline == "") {
-    //     titleLink = '<h3 class="newsroomArticleTitle card-title"><a href="' + fullTextLink + '">' + articleTitle + '</a></h3>';
-    // } else {
-    //     titleLink = '<h3 class="newsroomArticleTitle card-title"><a href="' + fullTextLink + '">' + headline + '</a></h3>';
-    // }
-
-
-    /***
-     *  display byline only when provided 
-     * 
-     * */
-    // if (author == "") {
-    //     authorByLine = '<p class="byLine hidden">No Author Provided</p>';
-    // } else {
-    //     authorByLine = '<p class="byLine">By ' + author + '</p>';
-    // }
-    // if (photoCredit == "") {
-    //     photoCredit = '<p class="byLine hidden">No Photographer Provided</p>';
-    // } else {
-    //     photoCredit = '<p class="byLine">Image credit: ' + frontPageImageCredit + '</p>';
-    // }
-
-
-
-    /***
-     *  verify Main image and photo credits
-     * 
-     * */
-    // if (frontPageImage == "") {
-    //     imageString = '<span class="newsroomImageWrapper hidden">No Image Provided</span>';
-    //     // photoCredit = '<p class="byLine hidden">No Photographer Provided</p>';
-
-    // } else {
-    //     imageString = '<span class="newsroomImageWrapper"><img src="' + frontPageImage + '" class="articleImage card-img-top" alt="' + frontPageImageCaption + '" /></span>';
-    //     // photoCredit = '<p class="byLine hidden">No Photographer Provided</p>';
-    // }
-
-
-
-
-    /***
      *  Parse for image
      * 
      * */
@@ -265,21 +177,7 @@ try {
 
 
 
-
-    /***
-     *  Write the document once
-     * 
-     * */
-    // document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, beginningHTML));
-    // document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, anchorTag));
-
-    // document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, imageString));
-    // document.write('<div class="newsroomArticleBlurb container card-body"><div class="row px-0">');
-    // document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, titleLink));
-    // document.write('</div></div>'); // close newsroomArticleBlurb and row divs
-    // document.write(endingHTML);
-
-
+    
     /***
      *  write document once
      * 

f91582278fd04ec2aceb1164267c62ae6aba61a2
vicchimenti <victor.chimenti@gmail.com>
2021-11-16 10:46:16 -0800

    import methods

--- law-text-html-contact.js
+++ law-text-html-contact.js
@@ -1,3 +1,72 @@
+/***
+ *      @author Victor Chimenti, MSCS
+ *      @see Seattle University School of Law Newsroom
+ *      @file law-text-html-contact.js
+ *          Law - Newscenter Contact
+ *          ID: 5603
+ * 
+ *      A contact box exclusively for the Law School News Center
+ *          Contains contact info for two profiles
+ *
+ *      Document will write once when the page loads
+ *
+ *      @version 1.0
+ */
+
+
+
+
+
+
+
+
+/***
+ *      Import T4 Utilities
+ */
+ importClass(com.terminalfour.publish.utils.BrokerUtils);
+
+ 
+ 
+ 
+ 
+ /***
+  *      Extract values from T4 element tags
+  *      and confirm valid existing content item field
+  */
+ function getContentValues(tag) {
+     try {
+         let _tag = BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, tag)
+         return {
+             isError: false,
+             content: _tag == '' ? null : _tag
+         }
+     } catch (error) {
+         return {
+             isError: true,
+             message: error.message
+         }
+     }
+ }
+ 
+ 
+ 
+
+ /***
+  *      Write the document
+  */
+ function writeDocument(array) {
+ 
+     for (let i = 0; i < array.length; i++) {
+ 
+         document.write(array[i]);
+     }
+ }
+
+
+
+
+
+
 <div class="newsCenterContact card">
   <h3 class="newsCenterContactHeader card-header"></h3>
   <div class="newsCenterContactBody card-body">

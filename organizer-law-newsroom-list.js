8b0ef1532d8378004cbcc1a0ba0c1a1ef595a9aa
vicchimenti <victor.chimenti@gmail.com>
2021-11-17 10:00:22 -0800

    visible title

--- organizer-law-newsroom-list.js
+++ organizer-law-newsroom-list.js
@@ -24,7 +24,7 @@ try {
   
 	header = '<div class="newsroomOrganizerWrapper contentItem col-xs-12 col-md-6 offset-md-3" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
 				<div class="titleWrapper standardContent col-xs-12">\
-                <h2 class="organizerTitle text-center sr-only">' + titleField + '</h2></div>\
+                <h2 class="organizerTitle text-center">' + titleField + '</h2></div>\
 				<div class="newsroomOrganizer standardContent card-deck">';
 	midder = '';
 	footer = '</div></div>';

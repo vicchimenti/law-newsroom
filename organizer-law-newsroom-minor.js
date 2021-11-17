3db0c0c3dedde9819adfa106532506d72cfe5f16
vicchimenti <victor.chimenti@gmail.com>
2021-11-17 09:24:06 -0800

    elevate title

--- organizer-law-newsroom-minor.js
+++ organizer-law-newsroom-minor.js
@@ -11,30 +11,33 @@ function readMedia(mediaID) {
 }
 
 try {
+
     // Import Organizer base from media library
     var base = readMedia(3050627);
     eval(String(base));
+
+    let titleField = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Title" output="selective-output" modifiers="striptags,htmlentities" format="$value" />');
+
     var header, midder, footer;
     var choice = String(content.get('Article type').publish());
+
     switch (choice) {
         default: header = '\
 			<div class="newsroomOrganizerWrapper minor-feed col-12 col-xs-12 order-xs-3 col-md-4 col-md-pull-8 order-md-2" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
+                <div class="titleWrapper standardContent col-xs-12">\
+                <h2 class="organizerTitle text-center sr-only">' + titleField + '</h2></div>\
 				<div class="newsroomOrganizer standardContent card-deck border-0">\
-                  <div class="newsroomOrganizerExtra"></div>\
+                <div class="newsroomOrganizerExtra"></div>\
 		';
-        midder = '\
- 		';
-        footer = '\
-			</div>\
-		</div>\
- 		';
+        midder = '';
+        footer = '</div></div>';    
     }
+
+
     // Write content
-    // Delegate header/footer writing to main method
-    // (paginator doesn't display them in publish on pages >1, only preview)
-    //if (header) document.write(header);
     main(header, midder, footer);
-    //if (footer) document.write(footer);
+
+
 } catch (err) {
     document.write(err.message + err.stack);
 }

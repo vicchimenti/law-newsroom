5cdb8ed478614d2c534e987741bc9bedba5ed419
vicchimenti <victor.chimenti@gmail.com>
2021-11-17 09:28:34 -0800

    elevate title

--- organizer-law-newsroom-major.js
+++ organizer-law-newsroom-major.js
@@ -11,24 +11,26 @@ function readMedia(mediaID) {
 }
 
 try {
-    
+
     // Import Organizer base from media library
     var base = readMedia(3050627);
     eval(String(base));
+
+    let titleField = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Title" output="selective-output" modifiers="striptags,htmlentities" format="$value" />');
+
     var header, midder, footer;
     var choice = String(content.get('Article type').publish());
+
     switch (choice) {
         default: header = '\
 			<div class="newsroomOrganizerWrapper major-feed col-12 col-xs-12 order-xs-2 col-md-8 col-md-push-4 order-md-3" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
-					<div class="newsroomOrganizer standardContent card-deck border-0">\
-                  		<div class="newsroomOrganizerExtra"></div>\
+                <div class="titleWrapper standardContent col-xs-12">\
+                <h2 class="organizerTitle text-center sr-only">' + titleField + '</h2></div>\
+                <div class="newsroomOrganizer standardContent card-deck border-0">\
+                <div class="newsroomOrganizerExtra"></div>\
 		';
-        midder = '\
- 		';
-        footer = '\
-            </div>\
-		</div>\
- 		';
+        midder = '';
+        footer = '</div></div>';    
     }
 
 

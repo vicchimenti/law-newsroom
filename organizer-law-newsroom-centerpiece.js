35f9558391cf053eeede6180a1134568add056f4
vicchimenti <victor.chimenti@gmail.com>
2021-11-17 09:17:17 -0800

    mv title above parent

--- organizer-law-newsroom-centerpiece.js
+++ organizer-law-newsroom-centerpiece.js
@@ -15,11 +15,16 @@ try {
     var base = readMedia(3050627);
     eval(String(base));
 
+    let titleField = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Title" output="selective-output" modifiers="striptags,htmlentities" format="$value" />');
+
     // declare content wrappers
     var header, midder, footer;
 
     // declare content wrappers
-    header = '<div class="heroOrganizerWrapper contentItem col-12 col-xs-12 order-xs-1 col-md-12 order-md-first" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main"><div class="heroOrganizer standardContent card-deck border-0"><div class="heroOrganizerExtra"></div>';
+    header = '<div class="heroOrganizerWrapper contentItem col-12 col-xs-12 order-xs-1 col-md-12 order-md-first" id="id' + content.getID() + '" data-position-default="Main" data-position-selected="Main">\
+                <div class="titleWrapper standardContent col-xs-12">\
+                <h2 class="organizerTitle text-center sr-only">' + titleField + '</h2></div>\
+                <div class="heroOrganizer standardContent card-deck border-0"><div class="heroOrganizerExtra"></div>';
     midder = '';
     footer = '</div></div>';
 

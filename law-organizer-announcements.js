17ef43b0e0cad214fbf23aa144b9934d1e7504b0
vicchimenti <victor.chimenti@gmail.com>
2021-11-19 12:14:08 -0800

    ud

--- law-organizer-announcements.js
+++ law-organizer-announcements.js
@@ -10,7 +10,7 @@
  */
 var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
 var optionToTestFor = "announcements"; //edit this to change the option
-var contentTypeLayout = 'output/announcementsfeed'; //edit this to change the Content Layout to use for output
+var contentTypeLayout = 'output/categoryfeed'; //edit this to change the Content Layout to use for output
 var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
 
 

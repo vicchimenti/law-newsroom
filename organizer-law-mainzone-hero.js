f671b1d89d001cf96e628beb39c87accd61e7598
vicchimenti <victor.chimenti@gmail.com>
2021-12-09 08:34:51 -0800

    ud base file id

--- organizer-law-mainzone-hero.js
+++ organizer-law-mainzone-hero.js
@@ -12,7 +12,7 @@ function readMedia(mediaID) {
 
 try {
     // Import Organizer base from media library
-    var base = readMedia(3050627);
+    var base = readMedia(3405416);
     eval(String(base));
 
     let titleField = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Title" output="selective-output" modifiers="striptags,htmlentities" format="$value" />');

608771e817549efde0cbeedb1970fc955b7fb233
vicchimenti <victor.chimenti@gmail.com>
2022-01-14 10:43:51 -0800

    ud logic

--- law-grid-organizer-archives.js
+++ law-grid-organizer-archives.js
@@ -21,7 +21,7 @@
  try {
  
      /* if content exists, it'll start at 0 or later, so process this */
-     if ((contentTypeLayout)) {
+     if ((contentTypeLayout == "output/gridfeed")) {
          var sw = new java.io.StringWriter();
          var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
          new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview);

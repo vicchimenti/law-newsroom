48b699adf30123c2ac9d9684e4c7342d951af9ef
vicchimenti <chimenti@seattleu.edu>
2023-05-24 12:23:43 -0700

    udr

--- sharethis-script.js
+++ sharethis-script.js
@@ -1,2 +1,5 @@
-< script src = 'https://platform-api.sharethis.com/js/sharethis.js#property=646e37e98b790100199498b0&product=sop'
+/* Begin Share This */ <
+script type = 'text/javascript'
+src = 'https://platform-api.sharethis.com/js/sharethis.js#property=646e37e98b790100199498b0&product=sop'
 async = 'async' > < /script>
+/* End Share This */

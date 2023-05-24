702fb24b024d585ab2c01352044da2e20ee069c9
vicchimenti <chimenti@seattleu.edu>
2023-05-24 11:38:55 -0700

    ud

--- sharethis-script.js
+++ sharethis-script.js
@@ -1,3 +1,2 @@
-< script type = 'text/javascript'
-src = 'https://platform-api.sharethis.com/js/sharethis.js#property=646e37e98b790100199498b0&product=sop'
+< script src = 'https://platform-api.sharethis.com/js/sharethis.js#property=646e37e98b790100199498b0&product=sop'
 async = 'async' > < /script>
